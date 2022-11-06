3699d86b4bf01b3e1c570ed3076f90880f4b66d3
374007 <59527666+374007@users.noreply.github.com>
2022-11-06 13:02:44 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+
 bool valid_octet (std::string octet) {
-    if ( int octet )
+    bool result = true;
+    if (octet.length() <= 3 && octet != "") {
+        for (int i = 0; i < octet.length(); i++) {
+            if (octet[i] < '0' || octet[i] > '9') {
+                result = false;
+                break;
+            }
+        }
+        if (result) {
+            if (std::stoi(octet) == 0 && octet.length() > 1 || std::stoi(octet) < 0 || std::stoi(octet) > 255) {
+                result = false;
+            }
+        }
+    }
+    else result = false;
+    return result;
 }
+
 std::string get_address_part (std::string ip, int i) {
     int index = 0;
     std::string octet = "" ;
@@ -11,8 +28,8 @@ std::string get_address_part (std::string ip, int i) {
             octet += ip [j];
         }
         else if ( ip[j] == '.') {
-           if ( j==0 || j == ip.length()-1 ) {
-               octet = "";
+           if ( j == 0 || j == ip.length()-1 || index == 3 || octet=="") {
+               octet = "-";
                break;
            }
            if (index == i) break;
@@ -22,20 +39,28 @@ std::string get_address_part (std::string ip, int i) {
            }
         }
         else {
-           octet = "" ;
+           octet = "-" ;
            break;
         }
     }
-
-    return octet;
+    if (valid_octet (octet))  return octet ;
+    else return "-";
 }
+
 int main() {
     std::string ip;
+    bool valid = true;
     std::cout << "Input IP Address!" << std::endl;
     std::cin >> ip;
-    std::cout << get_address_part(ip,0)<<"\n";
-    std::cout << get_address_part(ip,1)<<"\n";
-    std::cout << get_address_part(ip,2)<<"\n";
-    std::cout << get_address_part(ip,3)<<"\n";
+    for (int i = 0; i < 3 ; i++) {
+        if (get_address_part(ip, i) == "-") {
+            valid = false;
+            break;
+        }
+    }
+    if ( valid ) std::cout << "VALID!"<<"\n";
+    else  std::cout << "NOT VALID!"<<"\n";
+
+
     return 0;
 }
